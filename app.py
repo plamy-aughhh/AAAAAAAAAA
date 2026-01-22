e4bd3d0e516c2054fddb6b6e903e1fb5459de003
plamy-aughhh <pdstoyankova24a@gmail.com>
2026-01-22 10:46:10 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+st.title("App name")
+name = st.text_input("Enter a name")
+if st.button ("Check")
+if name strip () == "" :
+st.warning ("Please enter text")
+elif not name is alpha ():
+st.warning ("Warning")
+else st.success ("The text was entered correctly")
